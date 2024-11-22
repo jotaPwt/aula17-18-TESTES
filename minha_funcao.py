c1cce3cbaf33396faf4a774a4aeec38a6ccc555e
João Pedro Souza W. <158215503+jotaPwt@users.noreply.github.com>
2024-11-22 18:01:41 -0300

    Add files via upload

---
+++ minha_funcao.py
@@ -0,0 +1,7 @@
+def soma(a,b):
+    soma =  a + b
+    return soma
+
+def sub(a,b):
+    sub = a - b 
+    print(soma)
---
+++ test_minhas_funcoes.py
@@ -0,0 +1,8 @@
+from minha_funcao import *
+
+def testando_fucao():
+    assert soma(5, 10) == 15
+    assert soma(5, 5) == 10
+    assert soma(6, 4) == 10
+
+testando_fucao()
